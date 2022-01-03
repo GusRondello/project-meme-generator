bc7849e311c63136c75038e84c16257f096be5fc
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-03 18:00:35 -0300

    requisitos obrigatorios feitos

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 window.onload = function () {
   const inputText = document.getElementById('text-input');
+  const memeImage = document.getElementById('meme-image');
+  const inputImage = document.getElementById('meme-insert');
   function textoMeme() {
     const memeText = document.getElementById('meme-text');
     memeText.innerHTML = inputText.value;
   }
 
+  function addImage() {
+    // https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+    memeImage.src = URL.createObjectURL(inputImage.files[0]);
+  }
+
   inputText.addEventListener('input', textoMeme);
+  inputImage.addEventListener('change', addImage);
 };
